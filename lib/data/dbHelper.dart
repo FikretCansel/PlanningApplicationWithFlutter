d93c0b9f75836bce2a858203f966b287ae1aeb10
fikret <61146337+FikretCansel@users.noreply.github.com>
2020-10-20 17:26:31 +0300

    Update dbHelper.dart

--- lib/data/dbHelper.dart
+++ lib/data/dbHelper.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:ebrarinplanlari/models/Plan.dart';
+import 'package:[PROJECTNAME]/models/Plan.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
